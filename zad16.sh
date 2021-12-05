b35a719c2443f904549a4aa57e0518f0f1542be8
natalia787548 <73382792+natalia787548@users.noreply.github.com>
2021-12-05 18:30:36 +0100

    Create zad16.sh

---
+++ zad16.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd~
+
+echo "podaj nazwe uzytkownika" 
+read NAME
+
+echo "podaj nazwe grupy"
+read GROUP 
+
+
+if grep -q $GROUP /etc/group && grep -q $USER /etc/passwd
+then
+	echo "podane dane istnieja"
+else
+	groupadd $GROUP
+	useradd $NAME
+	usermod -a -G $GROUP $NAME	
+	passwd $NAME
+	ls /home
+userdel $NAME
+groupdel $GROUP
